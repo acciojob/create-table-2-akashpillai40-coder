7f827fb9f9aad682e9beda0514bc6067d856ffb0
Akash <akashpillai40@gmail.com>
2026-04-18 17:24:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 function createTable() {
     //Write your code here
-  
+	let table = document.getElementById("myTable");
+
+	//prompt
+	let rn = prompt("Input number of rows");
+	let cn = prompt("Input number of columns");
+    cn = Number(cn);
+	rn = Number(rn);
+
+	//check the rn & cn are 'number' & '>0'
+     if(isNaN(rn) || isNaN(cn)){
+		 return;
+	 }
+	
+	if(cn<0 || rn<0){
+		alert("Please enter a positive value")
+		return;
+	}
+
+	//creating table
+	
+    for(let i=0; i<rn; i++){
+		let tr = document.createElement("tr");
+		for(let j=0; j<cn; j++){
+			let td = document.createElement("td");
+			td.textContent = `Row-${i} Column-${j}`;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
 }
